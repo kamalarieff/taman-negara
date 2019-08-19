2905c6a5ddd79753ddf1ee11027a83a0425d2bec
Kamal Arieff <kamal.arieff@gmail.com>
2019-08-20 02:36:08 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { data } from "./data";
+import Container from "@material-ui/core/Container";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TextField from "@material-ui/core/TextField";
+import Fuse from "fuse.js";
 
 function App() {
+  const temp = data.map(item => {
+    const names = item["Who"].split(",");
+    return { ...item, Who: [...names] };
+  });
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResult, setSearchResult] = useState(temp);
+  const fuse = new Fuse(temp, { keys: ["Who"], threshold: "0" });
+
+  const handleChange = e => {
+    const name = e.target.value;
+    setSearchTerm(name);
+    if (name === "") {
+      setSearchResult(temp);
+      return;
+    }
+
+    const result = fuse.search(name);
+    setSearchResult(result);
+  };
+
+  console.log("searchResult", searchResult);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md">
+      <TextField
+        id="outlined-name"
+        label="Name"
+        margin="normal"
+        variant="outlined"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Day</TableCell>
+            <TableCell>Time</TableCell>
+            <TableCell>Where</TableCell>
+            <TableCell>Task</TableCell>
+            <TableCell>Who</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {searchResult.map(({ Day, Time, Where, Task, Who }) => {
+            return (
+              <TableRow>
+                <TableCell>{Day}</TableCell>
+                <TableCell>{Time}</TableCell>
+                <TableCell>{Where}</TableCell>
+                <TableCell>{Task}</TableCell>
+                <TableCell>
+                  {Who.map(name => (
+                    <p>{name}</p>
+                  ))}
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Container>
   );
 }
 
---
+++ src/data/index.js
@@ -0,0 +1,182 @@
+export const data = [
+  {
+    Day: 1,
+    Time: "08:00",
+    Where: "",
+    Task: "Luggage (Load)",
+    Who: "Jihan"
+  },
+  {
+    Day: 1,
+    Time: "10:00",
+    Where: "",
+    Task: "Luggage (unload)",
+    Who: "Gan"
+  },
+  {
+    Day: 1,
+    Time: "",
+    Where: "",
+    Task: "Assist to climb boat",
+    Who: "Denise,Suan"
+  },
+  {
+    Day: 1,
+    Time: "",
+    Where: "",
+    Task: "Staircase",
+    Who: "Priyanka"
+  },
+  {
+    Day: 1,
+    Time: "",
+    Where: "",
+    Task: "Direct to Restaurant",
+    Who: "Shahriman"
+  },
+  {
+    Day: 1,
+    Time: "",
+    Where: "",
+    Task: "In Restaurant ",
+    Who: "Chung"
+  },
+  {
+    Day: 1,
+    Time: "",
+    Where: "",
+    Task: "Reception",
+    Who: "Salin,Bee Yen,Suzana"
+  },
+  {
+    Day: 1,
+    Time: "",
+    Where: "",
+    Task: "At Team Building ",
+    Who: "Salin,Suzana,Bee Yen,Wai Hong,Jihan,Denise,Kamal,Adira,Gan"
+  },
+  {
+    Day: 1,
+    Time: "",
+    Where: "",
+    Task: "Reception",
+    Who: "Suan,Sudheer,Priyanka,Chung,Shahriman"
+  },
+  {
+    Day: 1,
+    Time: "",
+    Where: "",
+    Task: "RiverBank",
+    Who: "Salin,Suzana,Bee Yen,Kamal,Gan,Musician"
+  },
+  {
+    Day: 1,
+    Time: "",
+    Where: "",
+    Task: "Direct to Jetty",
+    Who: "Suan "
+  },
+  {
+    Day: 1,
+    Time: "",
+    Where: "",
+    Task: "Jetty",
+    Who: "Shahriman,Wai Hong"
+  },
+  {
+    Day: 1,
+    Time: "",
+    Where: "",
+    Task: "Jetty to Reception",
+    Who: "Shahriman,Wai Hong,Sudheer,Kamal,Gan"
+  },
+  {
+    Day: 1,
+    Time: "",
+    Where: "",
+    Task: "Clear Riverbank",
+    Who: "Salin,Suzana,Bee Yen,Adira"
+  },
+  {
+    Day: 2,
+    Time: "",
+    Where: "",
+    Task: "Mr & Mrs Deco",
+    Who: "Shukri,Suan,Salin,Suzana,Priyanka,Adira"
+  },
+  {
+    Day: 2,
+    Time: "",
+    Where: "",
+    Task: "Restaurant to Jetty",
+    Who: "Wai Hong,Shahriman"
+  },
+  {
+    Day: 2,
+    Time: "",
+    Where: "",
+    Task: "Tualang",
+    Who:
+      "Salin,Suzana,Bee Yen,Wai Hong,Jihan,Denise,Kamal,Adira,Gan,Suan,Priyanka,Shahriman,Chung,Sudheer"
+  },
+  {
+    Day: 2,
+    Time: "",
+    Where: "",
+    Task: "Fish Sanctuary",
+    Who:
+      "Salin,Suzana,Bee Yen,Wai Hong,Jihan,Denise,Kamal,Adira,Gan,Suan,Priyanka,Shahriman,Chung,Sudheer"
+  },
+  {
+    Day: 2,
+    Time: "",
+    Where: "",
+    Task: "Lata Berkoh",
+    Who:
+      "Salin,Suzana,Bee Yen,Wai Hong,Jihan,Denise,Kamal,Adira,Gan,Suan,Priyanka,Shahriman,Chung,Sudheer"
+  },
+  {
+    Day: 2,
+    Time: "",
+    Where: "",
+    Task: "Back to Resort",
+    Who: "Suzana,Salin,Jihan,Adira"
+  },
+  {
+    Day: 3,
+    Time: "",
+    Where: "",
+    Task: "To Jetty",
+    Who: "Salin,Suzana,Priyanka,Adira,Jihan,Denise,Suan"
+  },
+  {
+    Day: 3,
+    Time: "",
+    Where: "",
+    Task: "Load Things in Boat",
+    Who: "Wai Hong,Shahriman,Gan,Sudheer,Kamal"
+  },
+  {
+    Day: 3,
+    Time: "",
+    Where: "",
+    Task: "Unload things to Org Asli",
+    Who: "Wai Hong,Kamal,Gan,Shahriman,Chung,Sudheer"
+  },
+  {
+    Day: 3,
+    Time: "",
+    Where: "",
+    Task: "Bamboo Rafting",
+    Who:
+      "Salin,Suzana,Bee Yen,Wai Hong,Jihan,Denise,Kamal,Adira,Gan,Suan,Priyanka,Shahriman,Chung,Sudheer"
+  },
+  {
+    Day: 3,
+    Time: "",
+    Where: "",
+    Task: "Jetty to Rooms",
+    Who:
+      "Salin,Suzana,Bee Yen,Wai Hong,Jihan,Denise,Kamal,Adira,Gan,Suan,Priyanka,Shahriman,Chung,Sudheer"
+  }
+];
