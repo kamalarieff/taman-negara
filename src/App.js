b903c04051f38955ada43260ff4c96c95633fb89
Kamal Arieff <kamal.arieff@gmail.com>
2019-08-21 23:54:17 +0800

    restructure directory of volunteers

--- src/App.js
+++ src/App.js
@@ -1,77 +1,32 @@
 import React, { useState } from "react";
 import "./App.css";
-import { data } from "./data";
 import Container from "@material-ui/core/Container";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import TextField from "@material-ui/core/TextField";
-import Fuse from "fuse.js";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Drawer from "@material-ui/core/Drawer";
+import Volunteer from "./modules/volunteers";
 
 function App() {
-  const temp = data.map(item => {
-    const names = item["Who"].split(",");
-    return { ...item, Who: [...names] };
-  });
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchResult, setSearchResult] = useState(temp);
-  const fuse = new Fuse(temp, { keys: ["Who"], threshold: "0" });
-
-  const handleChange = e => {
-    const name = e.target.value;
-    setSearchTerm(name);
-    if (name === "") {
-      setSearchResult(temp);
-      return;
-    }
-
-    const result = fuse.search(name);
-    setSearchResult(result);
-  };
-
-  console.log("searchResult", searchResult);
+  const toggleDrawer = () => setDrawerOpen(!drawerOpen);
   return (
-    <Container maxWidth="md">
-      <TextField
-        id="outlined-name"
-        label="Name"
-        margin="normal"
-        variant="outlined"
-        value={searchTerm}
-        onChange={handleChange}
-      />
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Day</TableCell>
-            <TableCell>Time</TableCell>
-            <TableCell>Task</TableCell>
-            <TableCell>Who</TableCell>
-            <TableCell>Note</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {searchResult.map(({ Day, Time, Task, Who, Note }) => {
-            return (
-              <TableRow>
-                <TableCell>{Day}</TableCell>
-                <TableCell>{Time}</TableCell>
-                <TableCell>{Task}</TableCell>
-                <TableCell>
-                  {Who.map(name => (
-                    <p>{name}</p>
-                  ))}
-                </TableCell>
-                <TableCell>{Note}</TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </Container>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" onClick={toggleDrawer}>
+            Menu
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer open={drawerOpen} onClose={toggleDrawer}>
+        <p>Hello</p>
+      </Drawer>
+      <Container maxWidth="md">
+        <Volunteer />
+      </Container>
+    </>
   );
 }
 
---
+++ src/modules/volunteers/index.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { data } from "../../data";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TextField from "@material-ui/core/TextField";
+import Fuse from "fuse.js";
+
+export default () => {
+  const temp = data.map(item => {
+    const names = item["Who"].split(",");
+    return { ...item, Who: [...names] };
+  });
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResult, setSearchResult] = useState(temp);
+  const fuse = new Fuse(temp, { keys: ["Who"], threshold: "0" });
+
+  const handleChange = e => {
+    const name = e.target.value;
+    setSearchTerm(name);
+    if (name === "") {
+      setSearchResult(temp);
+      return;
+    }
+
+    const result = fuse.search(name);
+    setSearchResult(result);
+  };
+
+  return (
+    <>
+      <TextField
+        id="outlined-name"
+        label="Name"
+        margin="normal"
+        variant="outlined"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Day</TableCell>
+            <TableCell>Time</TableCell>
+            <TableCell>Task</TableCell>
+            <TableCell>Who</TableCell>
+            <TableCell>Note</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {searchResult.map(({ Day, Time, Task, Who, Note }) => {
+            return (
+              <TableRow>
+                <TableCell>{Day}</TableCell>
+                <TableCell>{Time}</TableCell>
+                <TableCell>{Task}</TableCell>
+                <TableCell>
+                  {Who.map(name => (
+                    <p>{name}</p>
+                  ))}
+                </TableCell>
+                <TableCell>{Note}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </>
+  );
+};
